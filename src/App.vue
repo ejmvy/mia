6525b862837d7e799a07eb8469552445bb370c5e
EJ McVey <ej.mcvey@geopal-solutions.com>
2020-09-23 15:59:51 +0100

    First Changes

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,72 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Header></Header>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from "./components/Header.vue";
+// import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    Header
   }
-}
+};
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+} */
+
+body {
+  background: #30d5c8;
+  font-family: "Kodchasan", sans-serif;
+  margin: 0;
+  padding: 0;
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
+}
+
+h1 {
+  font-family: "Roboto Slab", serif;
+  color: #663399;
+  font-size: 35px;
+}
+
+h2 {
+  color: #7852a9;
+  font-family: "Kodchasan", sans-serif;
+  text-transform: uppercase;
+  font-size: 30px;
+}
+
+h3 {
+  color: #7852a9;
+  font-family: "Roboto Slab", serif;
+  font-size: 25px;
+}
+
+h4 {
+  font-family: "Roboto Slab", serif;
+  color: #7852a9;
+}
+
+p {
+  line-height: 2;
+  font-size: 15px;
+}
+
+button {
+  text-transform: uppercase;
+  font-family: "Kodchasan", sans-serif;
+  letter-spacing: 3px;
+  font-size: 14px;
+  cursor: pointer;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,94 @@
+<template>
+  <header>
+    <div class="logo">
+      <a href="#">
+        <img id="logoPic" src="https://i.ibb.co/q7J3TBg/imageonline-co-whitebackgroundremoved.png" />
+      </a>
+    </div>
+    <nav>
+      <li>
+        <a href="classes.html">Classes</a>
+      </li>
+      <li>
+        <a href="commission.html">Commission</a>
+      </li>
+      <li>
+        <a href="contact.html">Contact</a>
+      </li>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+header {
+  position: fixed;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  height: 80px;
+  width: 100%;
+  -webkit-box-pack: justify;
+  -ms-flex-pack: justify;
+  justify-content: space-between;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+  z-index: 6;
+}
+
+header #logoPic {
+  width: 100px;
+  height: 50px;
+  z-index: 5;
+  margin-left: 30px;
+}
+
+header nav {
+  position: absolute;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  z-index: 5;
+  right: 0;
+}
+
+header nav li {
+  list-style: none;
+  margin: 0 15px;
+  padding: 30px;
+  text-transform: uppercase;
+  font-family: "Kodchasan", sans-serif;
+  letter-spacing: 3px;
+}
+
+header nav li a {
+  z-index: 6;
+  color: #663399;
+  text-decoration: none;
+  cursor: pointer;
+}
+
+header nav li a:hover {
+  color: white;
+}
+
+#active {
+  border-bottom: 3px solid transparent;
+  -webkit-animation: underline 1s 1 forwards;
+  animation: underline 1s 1 forwards;
+}
+
+#active a {
+  color: white;
+}
+</style>
